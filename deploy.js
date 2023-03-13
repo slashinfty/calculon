5ce839c51277af2810ba8e71d2f552141eada080
Matt Braddock <matt.braddock@gmail.com>
2023-03-13 19:48:17 -0400

    deploy script

--- deploy.js
+++ deploy.js
@@ -9,6 +9,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
+        console.log(`Started reloading ${CommandBuilders.length} slash commands`);
         const result = await rest.put(
             Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID),
             { body: CommandBuilders }
