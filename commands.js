d96dd2fe21da59f3a726c32aed8f1b5c2c525c3f
Matt Braddock <matt.braddock@gmail.com>
2023-03-13 20:03:24 -0400

    header

--- commands.js
+++ commands.js
@@ -56,10 +56,18 @@ SlashCommands.set('search-emby', async interaction => {
         const userResponse = await fetch(`${process.env.EMBY_IP}/emby/Users/Public`);
         const userData = await userResponse.json();
         const userId = userData.find(user => user['Name'] === process.env.EMBY_NAME)['Id'];
-        const mediaResponse = await fetch(`${process.env.EMBY_IP}/emby/Users/${userId}/Views?IncludeExternalContent=false`);
+        const mediaResponse = await fetch(`${process.env.EMBY_IP}/emby/Users/${userId}/Views?IncludeExternalContent=false`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
         const mediaData = await mediaResponse.json();
         const mediaId = mediaData.find(media => media['Name'] === interaction.options.getString('type'))['Id'];
-        const itemResponse = await fetch(`${process.env.EMBY_IP}/emby/Users/${userId}/Items?ParentId=${mediaId}&api_key=${process.env.EMBY_API}`);
+        const itemResponse = await fetch(`${process.env.EMBY_IP}/emby/Users/${userId}/Items?ParentId=${mediaId}&api_key=${process.env.EMBY_API}`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
         const itemData = await itemResponse.json();
         const title = interaction.options.getString('title');
         const itemResult = itemData.filter(item => item['Name'].toLowerCase().includes(title.toLowerCase()));
